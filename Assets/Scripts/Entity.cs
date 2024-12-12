7caf1715470f60bf11599f5d587caaed734dc156
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-12-11 19:09:36 -0500

    changed scripts to work with inheritence

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Entity : MonoBehaviour {
+
+    [Header("Ground collosion")]
+    [SerializeField] protected LayerMask groundLayer;
+    [SerializeField] protected float groundCheckDistance;
+
+
+    protected Rigidbody2D rigidbody2D;
+    protected Collider2D collider2D;
+    protected Animator animator;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<Collider2D>();
+        animator = GetComponentInChildren<Animator>();
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update() {
+
+    }
+
+    protected virtual bool IsGrounded() {
+        return Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0, Vector2.down, groundCheckDistance, groundLayer);
+
+    }
+
+    protected virtual void FlipSpriteFacingDirection() {
+        //if (xInput > 0.01f) {
+        if (rigidbody2D.linearVelocityX > 0.01f) {
+            transform.localScale = Vector3.one;
+
+            //} else if (xInput < -0.01f) {
+        } else if (rigidbody2D.linearVelocityX < -0.01f) {
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        }
+    }
+
+    protected virtual void OnDrawGizmos() {
+        Gizmos.DrawCube(new Vector3(collider2D.bounds.center.x, collider2D.bounds.center.y - collider2D.bounds.size.y / 2), new Vector3(collider2D.bounds.size.x, groundCheckDistance));
+
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : Entity {
 
     [Header("Player Parameters")]
     [SerializeField] private float moveSpeed;
@@ -20,29 +20,21 @@ public class PlayerScript : MonoBehaviour {
     private int comboCounter;
 
 
-    [Header("Ground collosion")]
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float groundCheckDistance;
-
-
-    private Rigidbody2D rb;
-    private CapsuleCollider2D collider2D;
-    private Animator animator;
 
     private float xInput;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        collider2D = GetComponent<CapsuleCollider2D>();
-        animator = GetComponentInChildren<Animator>();
+    protected override void Start() {
+        base.Start();
 
     }
 
 
     // Update is called once per frame
-    void Update() {
+    protected override void Update() {
+        base.Update();
+
         PlayerInput();
 
         PlayerMovement();
@@ -57,13 +49,7 @@ public class PlayerScript : MonoBehaviour {
     private void PlayerInput() {
         xInput = Input.GetAxis("Horizontal");
 
-        if (xInput > 0.01f) {
-            transform.localScale = Vector3.one;
-
-        } else if (xInput < -0.01f) {
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        }
+        FlipSpriteFacingDirection();
 
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
             isAttacking = true;
@@ -77,13 +63,15 @@ public class PlayerScript : MonoBehaviour {
     }
 
 
+
+
     private void PlayerMovement() {
         if (dashTime > 0) {
             //rb.linearVelocityX = xInput * dashSpeed;
-            rb.linearVelocity = new Vector2(xInput * dashSpeed, 0);
+            rigidbody2D.linearVelocity = new Vector2(transform.localScale.x * dashSpeed, 0);
 
         } else {
-            rb.linearVelocityX = xInput * moveSpeed;
+            rigidbody2D.linearVelocityX = xInput * moveSpeed;
 
         }
 
@@ -98,19 +86,8 @@ public class PlayerScript : MonoBehaviour {
 
 
     private void PlayerJump() {
-        rb.linearVelocityY = jumpForce;
+        rigidbody2D.linearVelocityY = jumpForce;
 
-    }
-
-
-    private bool IsGrounded() {
-        return Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0, Vector2.down, groundCheckDistance, groundLayer);
-
-    }
-
-
-    private void FlipPlayer() {
-        transform.Rotate(0, 180, 0);
     }
 
 
@@ -134,17 +111,11 @@ public class PlayerScript : MonoBehaviour {
 
         animator.SetBool("IsDashing", dashTime > 0);
 
-        animator.SetFloat("yVelocity", rb.linearVelocityY);
+        animator.SetFloat("yVelocity", rigidbody2D.linearVelocityY);
 
         animator.SetBool("IsAttacking", isAttacking);
 
         animator.SetInteger("ComboCounter", comboCounter);
-
-    }
-
-
-    private void OnDrawGizmos() {
-        Gizmos.DrawCube(new Vector3(collider2D.bounds.center.x, collider2D.bounds.center.y - collider2D.bounds.size.y / 2), new Vector3(collider2D.bounds.size.x, groundCheckDistance));
 
     }
 
